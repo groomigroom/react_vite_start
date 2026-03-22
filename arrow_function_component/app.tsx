479b7fbe8550d4d8b7453406b9d40b015cf375db
groomigroom <googjohn0143@gmail.com>
2026-03-22 14:20:41 +0900

    Create app.tsx

---
+++ arrow_function_component/app.tsx
@@ -0,0 +1,9 @@
+import ArrowComponent from "./component/arrow_component"
+
+export default function App() {
+  return (
+    <div>
+      <ArrowComponent href="https://www.naver.com" text="네이버요" />
+    </div>
+  )
+}
