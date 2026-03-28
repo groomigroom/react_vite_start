b676846620c0c3d1745a617c1a2510b08ffb61fc
groomigroom <googjohn0143@gmail.com>
2026-03-28 16:58:02 +0900

    Update OnClick.tsx

--- SyntheticEvent/OnClick.tsx
+++ SyntheticEvent/OnClick.tsx
@@ -3,12 +3,12 @@ import type {SyntheticEvent} from 'react';
 export default function ReactOnClick() {
     const onClick = (e: SyntheticEvent) => {
         const {isTrusted, target, bubbles} = e;
-        console.log('mouse click occurs on <button>', isTrusted, target, bubbles);
+        console.log('마우스 클릭 <button>', isTrusted, target, bubbles);
     }
     return (
         <div>
             <p>ReactOnClick</p>
-            <button onClick={onClick}>Click Me</button>
+            <button onClick={onClick}>클릭하세요</button>
         </div>
     )
 }
