67b188fe67c521971b009fa546a69eb1f4b84412
groomigroom <googjohn0143@gmail.com>
2026-05-28 10:44:38 +0900

    Create function_jsx.jsx

---
+++ function_jsx.jsx
@@ -0,0 +1,24 @@
+function add(num1, num2) {
+  console.log(num1 + num2);
+}
+
+add(1, 2);
+
+function showshow(arr) {
+  arr.forEach((item) => {
+    console.log("김구름의" + item);
+  });
+}
+
+showshow([1, 2, 3, 4]);
+
+function App() {
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
